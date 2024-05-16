2c5268019663710fbfda5bac52421ff9947a07fc
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-05-16 23:48:25 +0500

    Create palindromeNum.dart

---
+++ palindromeNum.dart
@@ -0,0 +1,14 @@
+bool isPalindrome(int number) {
+  String numberString = number.toString();
+  String reversedString = numberString.split('').reversed.join('');
+  return numberString == reversedString;
+}
+
+void main() {
+  int number = 12321;
+  if (isPalindrome(number)) {
+    print('$number is a palindrome.');
+  } else {
+    print('$number is not a palindrome.');
+  }
+}
