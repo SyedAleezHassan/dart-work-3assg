914f3838fb21c462dd78d3c3203ed75d2287c253
SyedAleezHassan <syedaleezhassan@gmal.com>
2024-05-18 17:15:23 +0500

    new practice file

---
+++ prac.dart
@@ -0,0 +1,12 @@
+void main() {
+  // bool x;
+  // num A = 2, B = 5;
+  // x = (A == 2 && B == 5) && (A > 2 || B == 5);
+  // print(x);
+  List Names = ["syed", "aleez", 'hassan'];
+  Names.add("abc");
+  print(Names);
+  String name = "syed aleez hassan";
+  num lengthOfName = name.length;
+  print(lengthOfName);
+}
