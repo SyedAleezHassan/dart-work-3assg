20d874c4357a482206a0dffd50efd7963207cac1
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-06-05 09:30:07 +0500

    Create saveLogin.dart

---
+++ saveLogin.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  bool isBool = false;
+  List<Map> loginAttempts = [];
+  while (isBool == false) {
+    String email = stdin.readLineSync()!;
+    String pass = stdin.readLineSync()!;
+    loginAttempts.add({"email": email, "password": pass});
+    if (email == "aleez" && pass == "1212") {
+      print("loged in sucuessfully");
+      isBool = true;
+    } else {
+      print("login failed");
+    }
+  }
+  print(loginAttempts);
+}
