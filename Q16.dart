2166d90e37f78b2858420f548899114b8596e682
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-06-30 14:15:11 +0500

    Update Q16.dart

--- Q16.dart
+++ Q16.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<int> originalList = [1, 2, 3, 4, 5];
+  List<int> originalList = [1, 2, 3, 4, 5,6];
   List<int> evenNumbers = originalList.where((num) => num % 2 == 0).toList();
   print(evenNumbers);
 }
