e6782c9c1a8583a842a11ac85604167543548821
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-07-14 22:22:21 +0500

    Update Q11.dart

--- Q11.dart
+++ Q11.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<int> originalList = [1, 2, 3, 4, 5.6];
+  List<int> originalList = [1, 2, 3, 4, 5.6,9];
   int n = 3;
   List<int> newList = originalList.take(n).toList();
   print(newList);
