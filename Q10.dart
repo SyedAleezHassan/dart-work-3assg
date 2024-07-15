ec7913e4d613fdd32adc0fea6c90bbcbadb7b582
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-07-15 12:27:22 +0500

    Update Q10.dart

--- Q10.dart
+++ Q10.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<String> words = ['apple', 'banana', 'apple', 'orange', 'kiwi', 'banana'];
+  List<String> words = ['apple', 'banana', 'apple', 'orange', 'kiwi', 'banana' , 'strawberry'];
   List<String> uniqueWords = words.toSet().toList();
   print(uniqueWords);
 }
